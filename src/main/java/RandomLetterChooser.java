dd937901263a20a5e4b70a5ceb6e639a1282e30d
jenny-kwan <jennykwan222@gmail.com>
2025-04-24 11:41:30 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,7 +4,8 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
-		
+   		super(getSingleLetters(str));
+	}
 	}
 	
 	
